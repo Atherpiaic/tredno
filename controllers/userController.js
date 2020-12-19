af85f6f8d61186911c68b78629447c73e16bdf23
ather <51886808+Atherpiaic@users.noreply.github.com>
2020-12-19 11:45:46 +0500

    Allow All

---
+++ controllers/userController.js
@@ -0,0 +1,75 @@
+const catchAsync = require('./../utils/catchAsync');
+const User = require('../models/userModel');
+const AppError = require('../utils/appError');
+
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {};
+  Object.keys(obj).forEach((el) => {
+    if (allowedFields.includes(el)) newObj[el] = obj[el];
+  });
+
+  return newObj;
+};
+
+exports.updateMe = catchAsync(async (req, res, next) => {
+  // 1) Create error if user POSTs password data
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(
+      new AppError(
+        'This route is not for password updates. Please use /UpdateMyPassword',
+        400
+      )
+    );
+  }
+
+  // 2) Updat user document
+  const filteredBody = filterObj(req.body, 'name', 'email');
+
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    status: 'success',
+  });
+});
+
+exports.getAllUsers = catchAsync(async (req, res, next) => {
+  const user = await User.find();
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user,
+    },
+  });
+});
+
+exports.getUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!',
+  });
+};
+
+exports.createUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!',
+  });
+};
+
+exports.updateUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!',
+  });
+};
+
+exports.deleteUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!',
+  });
+};
---
+++ utils/email.js
@@ -0,0 +1,23 @@
+const nodemailer = require('nodemailer');
+
+const sendEmail = async (option) => {
+  // 1) Create a transporter
+  const transporter = nodemailer.createTransport({
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
+    auth: {
+      user: process.env.EMAIL_USERNAME,
+      pass: process.env.EMAIL_PASSWORD,
+    },
+  });
+  // 2) Define the email options
+  const mailOption = {
+    from: 'Ather <ather@hello.io>',
+    to: option.email,
+    subject: option.subject,
+    text: option.message,
+  };
+  // 3) Actually send the email
+  await transporter.sendMail(mailOption);
+};
+module.exports = sendEmail;
